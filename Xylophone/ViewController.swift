84b9c61ee6b1d8363cae6c6fedf0c2e77f2d5ae1
Clayton Watkins <watkinclayton17@yahoo.com>
2020-07-30 18:05:50 -0700

    Created Function to play sound when button pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    // MARK: Properties
+    var player: AVAudioPlayer?
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // MARK: IBAction
     @IBAction func keyPressed(_ sender: UIButton) {
-      
+        playSoundC()
     }
     
+    // MARK: Private Functions
+    private func playSoundC() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            guard let player = player else { return }
+            player.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
 
